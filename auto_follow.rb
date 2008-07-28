706633cb7a6196eaa005120104fe138a484f5539
Andy Shen <andy@shenie.info>
2008-07-28 22:56:18 +1000

    ability to follow people from search result

--- auto_follow.rb
+++ auto_follow.rb
@@ -3,11 +3,13 @@
 require 'rubygems'
 gem "twitter", '>=0.2.6'
 require 'twitter'
+require 'hpricot'
 
 # 
 # Usage:
 # follower = AutoFollower.new('admin@example.com', 'password')
 # follower.start
+# follower.stalk('cincinnati')
 # 
 class AutoFollower
   
@@ -16,10 +18,27 @@ class AutoFollower
   end
   
   def start
-    friends = @twitter.friends(true).collect { |f| f.screen_name }
     followers = @twitter.followers(true).collect { |f| f.screen_name }
     
-    (followers - friends).each { |name| @twitter.create_friendship(name) }
+    (followers - friends).each { |name| follow(name) }
   end
 
+  def stalk(query, page = 1)
+    doc = Hpricot(open("http://twitter.com/search/users?q=#{query}&page=#{page}"))
+    names = doc.search(".screen_name span").collect { |d| d.innerHTML }
+    return if names.empty?
+    (names - friends).each { |name| follow(name) }
+    page = page + 1
+    stalk(query, page)
+  end
+
+  private
+  
+    def follow(name)
+      @twitter.create_friendship(name)
+    end
+    
+    def friends
+      @friends ||= @twitter.friends(true).collect { |f| f.screen_name }
+    end
 end
