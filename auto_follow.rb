45d0ffa3563616991febfca54952d69297bd303e
Andy Shen <andy@shenie.info>
2008-07-30 22:07:16 +1000

    added ability to black list people, added delay between each successive follow request, use logger and allow this file to be run as a script

--- auto_follow.rb
+++ auto_follow.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
+require "logger"
+require "yaml"
 gem "twitter", '>=0.2.6'
 require 'twitter'
 require 'hpricot'
@@ -11,23 +13,41 @@ require 'hpricot'
 # follower.start
 # follower.stalk('cincinnati')
 # 
+# Another option if you only want to follow your followers is
+# running it off command line like auto_follow.rb admin@example.com password
+# 
 class AutoFollower
   
+  FOLLOW_INTERVAL = 10
   TWITTER_PAGE_SIZE = 100
+  BLACK_LIST = 'black_list.yml'
   
-  def initialize(email, password)
+  attr_accessor :black_list
+  
+  def initialize(email, password, output = STDOUT, level = Logger::INFO)
     @twitter = Twitter::Base.new(email, password)
+    
+    output = eval(output) if output.is_a?(String)
+    level = eval(level) if level.is_a?(String)
+    
+    @log = Logger.new(output)
+    @log.sev_threshold = level
+    
+    @black_list = File.exist?(BLACK_LIST) ? YAML.load_file(BLACK_LIST) : []
   end
   
   def start
-    (followers - friends).each { |name| follow(name) }
+    peeps = (followers - friends).find_all { |n| !black_listed?(n) }
+    
+    @log.info "Need to follow #{peeps.size} people"
+    peeps.each { |name| follow(name) }
   end
 
   def stalk(query, page = 1)
     doc = Hpricot(open("http://twitter.com/search/users?q=#{query}&page=#{page}"))
     names = doc.search(".screen_name span").collect { |d| d.innerHTML }
     return if names.empty?
-    (names - friends).each { |name| follow(name) }
+    (names - friends).find_all { |n| !black_listed?(n) }.each { |name| follow(name) }
     page = page + 1
     stalk(query, page)
   end
@@ -42,19 +62,31 @@ class AutoFollower
 
   private
   
-    def follow(name)
+    def black_listed?(name)
+      black_list.include?(name)
+    end
+  
+    def follow(name, delay = FOLLOW_INTERVAL)
+      @log.info "Following: #{name}"
       @twitter.create_friendship(name)
+      sleep delay
     end
     
     def find_followers(page = 1)
+      @log.debug "followers page: #{page}"
       f = @twitter.followers(:lite => true, :page => page).collect { |f| f.screen_name }
       return f if f.empty? || f.size < TWITTER_PAGE_SIZE
       f + find_followers(page + 1)
     end
     
     def find_friends(page = 1)
+      @log.debug "friends page: #{page}"
       f = @twitter.friends(:lite => true, :page => page).collect { |f| f.screen_name }
       return f if f.empty? || f.size < TWITTER_PAGE_SIZE
       f + find_friends(page + 1)
     end
+end
+
+if $0 == __FILE__
+  AutoFollower.new(*ARGV).start
 end
